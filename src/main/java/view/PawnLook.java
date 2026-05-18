f6d07e4073d35d9528315d413c0629531479da34
gmathix <smegmuscarlsen@gmail.com>
2026-05-18 22:00:51 +0200

    improve pawn look with O for pawns and X for king

--- src/main/java/view/PawnLook.java
+++ src/main/java/view/PawnLook.java
@@ -18,19 +18,15 @@ public class PawnLook extends ElementLook {
         Pawn pawn = (Pawn)element;
 
         if (pawn.getColor() == Pawn.PAWN_MOSCOVITE) {
-            shape[0][0] = ConsoleColor.BLACK_BACKGROUND + ConsoleColor.YELLOW_BACKGROUND + 0 +
+            shape[0][0] = ConsoleColor.BLACK_BACKGROUND + ConsoleColor.YELLOW_BRIGHT + 0 +
                     ConsoleColor.RESET;
         } else if (pawn.getColor() == Pawn.PAWN_SOLDIER) {
-            shape[0][0] = ConsoleColor.BLACK_BACKGROUND + ConsoleColor.GREEN_BACKGROUND + 0 +
+            shape[0][0] = ConsoleColor.BLACK_BACKGROUND + ConsoleColor.GREEN_BRIGHT + 0 +
                     ConsoleColor.RESET;
         } else if (pawn.getColor() == Pawn.PAWN_KING) {
-            shape[0][0] = ConsoleColor.BLACK_BACKGROUND + ConsoleColor.BLUE_BACKGROUND_BRIGHT + 0 +
+            shape[0][0] = ConsoleColor.BLACK_BACKGROUND + ConsoleColor.GREEN_BOLD_BRIGHT + 'X' +
                     ConsoleColor.RESET;
         }
-        /*
-        TO FULFILL:
-            - put in shape[0][0] the pawn's value, char color = white on black, or black on red depending on the pawn color.
-         */
     }
 
 }
