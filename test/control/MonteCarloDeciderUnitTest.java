2722184800504a8d512efac9322bb21b4987b8cc
amecener-256 <antoine.mec.che@gmail.com>
2026-05-21 18:24:06 +0200

    begin writing unit tests

--- test/control/MonteCarloDeciderUnitTest.java
+++ test/control/MonteCarloDeciderUnitTest.java
@@ -1,4 +1,33 @@
 package control;
 
+import boardifier.control.ActionPlayer;
+import boardifier.control.Controller;
+import boardifier.model.GameElement;
+import boardifier.model.Model;
+import boardifier.model.action.ActionList;
+import control.algos.Board;
+import control.algos.NegaMonteCarlo;
+import model.Move;
+import model.TablutBoard;
+import model.TablutStageModel;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
 public class MonteCarloDeciderUnitTest {
+    @Test
+    public void TestDecide(){
+        TablutStageModel stage = Mockito.mock(TablutStageModel.class);
+        TablutBoard tablutBoard = Mockito.mock(TablutBoard.class);
+        Board board = Mockito.mock(Board.class);
+        NegaMonteCarlo negaMonteCarlo = Mockito.mock(NegaMonteCarlo.class);
+        Move bestMove = Mockito.mock(Move.class);
+
+
+        MonteCarloDecider monteCarloDecider = new MonteCarloDecider(new Model(), new Controller() {
+            @Override
+            public void stageLoop() {
+
+            }
+        })
+    }
 }
--- test/model/TablutStageFactoryUnitTest.java
+++ test/model/TablutStageFactoryUnitTest.java
@@ -1,3 +1,17 @@
 package model;
+
+import boardifier.model.GameStageModel;
+import boardifier.model.TextElement;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
 public class TablutStageFactoryUnitTest {
+    @Test
+    public void TestSetup(){
+        TextElement text = Mockito.mock(TextElement.class);
+        TablutBoard board = Mockito.mock(TablutBoard.class);
+        GameStageModel gameStageModel = Mockito.mock(GameStageModel.class)
+        TablutStageFactory tablutStageFactory = new TablutStageFactory(gameStageModel);
+
+    }
 }
